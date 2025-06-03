efdb3c9c2d39d3f0cd8c1ed830d5aa86fca4f4c9
Francois Raminosona <francois.raminosona@laerdal.com>
2025-06-03 16:52:54 +0200

    refa (Vibrations): Simplifies command property types
    Refactors command properties to use 'Command' instead of
    'ICommand' to remove unnecessary dependency and enhance
    code simplicity.
    
    Ensures the class remains a singleton and maintains its
    functionality for haptic feedback handling.
    
    No change in external behavior for users.

--- Maui-Developer-Sample/Pages/Vibrations/BindableHapticFeedback/BindableHapticFeedbackSimple.cs
+++ Maui-Developer-Sample/Pages/Vibrations/BindableHapticFeedback/BindableHapticFeedbackSimple.cs
@@ -1,33 +1,27 @@
-using System.Windows.Input;
-
 namespace Maui_Developer_Sample.Pages.Vibrations.BindableHapticFeedback;
 
 public class BindableHapticFeedbackSimple : BindableObject
 {
-    
+
     #region Singleton
 
-    private readonly static Lazy<BindableHapticFeedbackSimple> LazyInstance =
-        new Lazy<BindableHapticFeedbackSimple>(() => new BindableHapticFeedbackSimple());
+    private readonly static Lazy<BindableHapticFeedbackSimple> LazyInstance = new Lazy<BindableHapticFeedbackSimple>(() => new BindableHapticFeedbackSimple());
 
-    public static BindableHapticFeedbackSimple Instance
-    {
-        get => LazyInstance.Value;
-    }
+    public static BindableHapticFeedbackSimple Instance => LazyInstance.Value;
 
     #endregion
+
     private BindableHapticFeedbackSimple()
     {
         VibrateClickCommand = new Command(VibrateClick);
         VibrateLongPressCommand = new Command(VibrateLongPress);
     }
 
-    private void VibrateClick() =>
-        HapticFeedback.Default.Perform(HapticFeedbackType.Click);
+    private void VibrateClick() => HapticFeedback.Default.Perform(HapticFeedbackType.Click);
 
-    private void VibrateLongPress() =>
-        HapticFeedback.Default.Perform(HapticFeedbackType.LongPress);
+    private void VibrateLongPress() => HapticFeedback.Default.Perform(HapticFeedbackType.LongPress);
 
-    public ICommand VibrateClickCommand { get; }
-    public ICommand VibrateLongPressCommand { get; }
+    public Command VibrateClickCommand { get; }
+
+    public Command VibrateLongPressCommand { get; }
 }
