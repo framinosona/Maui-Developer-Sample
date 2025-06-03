57e8108e187b050b9b45babedf97cf0dde4f924f
Francois Raminosona <francois.raminosona@laerdal.com>
2025-06-03 13:48:10 +0200

    feat(MauiProgram): integrate Community Toolkit
    Replaces namespaces to incorporate the .NET MAUI Community Toolkit.
    Initializes the toolkit to enhance app capabilities and streamline
    development. Removes unused namespace imports to optimize imports.

--- Maui-Developer-Sample/MauiProgram.cs
+++ Maui-Developer-Sample/MauiProgram.cs
@@ -1,10 +1,6 @@
-﻿using Framinosona.HapticFeedback;
+﻿using CommunityToolkit.Maui;
 
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Maui.Controls.Hosting;
-using Microsoft.Maui.Devices;
-using Microsoft.Maui.Hosting;
 
 namespace Maui_Developer_Sample;
 
@@ -14,6 +10,8 @@ public static class MauiProgram
     {
         var builder = MauiApp.CreateBuilder();
         builder.UseMauiApp<App>()
+                // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+               .UseMauiCommunityToolkit()
                .ConfigureFonts(fonts => {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
