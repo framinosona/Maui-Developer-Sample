2d4e94a5111cdf0aa43ac60c7d617635d15389a5
Francois Raminosona <francois.raminosona@laerdal.com>
2025-07-09 02:10:30 +0200

    refa(UI): Simplifies ViewModel binding in AppTheme_Page
    Removes unnecessary private field by directly assigning
    the ViewModel to the BindingContext, simplifying constructor
    logic and reducing code redundancy.

--- Maui-Developer-Sample/Pages/UI/AppTheme_Page.xaml.cs
+++ Maui-Developer-Sample/Pages/UI/AppTheme_Page.xaml.cs
@@ -4,13 +4,12 @@ namespace Maui_Developer_Sample.Pages.UI;
 
 public partial class AppTheme_Page
 {
-    private readonly AppThemeViewModel _viewModel;
 
     public AppTheme_Page(AppThemeViewModel appThemeViewModel)
     {
         InitializeComponent();
-        _viewModel = appThemeViewModel ?? throw new ArgumentNullException(nameof(appThemeViewModel));
-        BindingContext = _viewModel;
+        var viewModel = appThemeViewModel ?? throw new ArgumentNullException(nameof(appThemeViewModel));
+        BindingContext = viewModel;
     }
 }
 
